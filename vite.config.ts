75e05260d68ca93641a2c9a8f4a8f1202105b481
Shayan Bali <shayanbali@gmail.com>
2025-12-15 12:40:32 +0000

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/insight-map',
+  base: '/video-mind',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
